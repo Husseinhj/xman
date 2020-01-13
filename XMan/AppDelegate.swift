b5f591538c8c68e29cbd36bdfad281e657dd1615
Hussein <Hussein.Juybari+github@gmail.com>
2020-01-13 17:58:10 +0330

    Initial Commit

---
+++ XMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XMan
+//
+//  Created by Hussein Habibi Juybari on 1/13/20.
+//  Copyright © 2020 Pluginator. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XMan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XMan
+//
+//  Created by Hussein Habibi Juybari on 1/13/20.
+//  Copyright © 2020 Pluginator. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
